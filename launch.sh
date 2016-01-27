3f7d423e56e9da323cd20aed77d13f9a568fdb31
Ben Hall <Ben@Benhall.me.uk>
2016-01-27 10:14:37 +0000

    Use a data container to store config. Attempt to auto find IP

--- launch.sh
+++ launch.sh
@@ -1,8 +1,7 @@
 echo 'Starting Cluster'
-echo 'If your client + Docker host are on different machines then $(pwd) will fail and Logstash will not be able to find the config file'
-echo 'Logspout needs the IP of your Docker host - Change 192.168.99.100 to reflect this'
+echo 'Assumes Docker host is called docker'
 
-docker rm -f  elk_es kibana logspout logstash
+docker rm -f  elk_es kibana logspout logstash logstash_config
 
 docker run -d \
   -p 9200:9200 \
@@ -10,11 +9,14 @@ docker run -d \
   --name elk_es \
   -e LOGSPOUT=ignore \
   elasticsearch:1.5.2
+  
+docker create -v /config --name logstash_config busybox
+docker cp logstash.conf logstash_config:/config/
 
 docker run -d \
   -p 5000:5000 \
   -p 5000:5000/udp \
-  -v $(pwd):/config \
+   --volumes-from logstash_config
   --link elk_es:elasticsearch \
   --name logstash \
   -e LOGSPOUT=ignore \
@@ -28,12 +30,13 @@ docker run -d \
   -e LOGSPOUT=ignore \
   kibana:4.1.2
 
+ip=$(ping -c 1 docker | awk -F'[()]' '/PING/{print $2}')
 docker run -d \
   -p 8000:8000 \
   -v /var/run/docker.sock:/tmp/docker.sock \
   --name logspout \
   -e DEBUG=true \
-  gliderlabs/logspout:master syslog://192.168.99.100:5000
+  gliderlabs/logspout:master syslog://$ip:5000
 
 docker run -d ubuntu bash -c 'for i in {0..60}; do echo $i; sleep 1; done'
 

74beba43c4a833116a8c7fa7a34beda1725b5ea5
Ben Hall <Ben@Benhall.me.uk>
2017-02-11 16:04:53 +0000

    Upgrade to ES 5.2.0

--- launch.sh
+++ launch.sh
@@ -2,25 +2,25 @@ echo 'Starting Cluster'
 docker rm -f elk_elasticsearch elk_kibana elk_logstash
 
 cat << EOF > Dockerfile-es
-FROM elasticsearch:5.1.1-alpine
+FROM elasticsearch:5.2.0-alpine
 
 COPY elasticsearch.yml /config/
 EOF
 
-docker build -f Dockerfile-es -t elasticsearch:5.1.1-alpine .
+docker build -f Dockerfile-es -t elasticsearch:5.2.0-alpine .
 
-docker run --name=elk_elasticsearch -d elasticsearch:5.1.1-alpine -Etransport.host=127.0.0.1
+docker run --name=elk_elasticsearch -d elasticsearch:5.2.0-alpine -Etransport.host=127.0.0.1
 docker run --name=elk_kibana --link elk_elasticsearch:elasticsearch -p 5601:5601 -d kibana:5.1.1
 
 cat << EOF > Dockerfile
-FROM logstash:5.1.1-alpine
+FROM logstash:5.2.0-alpine
 
 COPY logstash.conf /config/
 
 CMD ["-f", "/config/logstash.conf"]
 EOF
 
-docker build -t logstash-with-config:5.1.1 .
+docker build -t logstash-with-config:5.2.0 .
 
 docker run -d \
   -p 5000:5000 \
@@ -28,7 +28,7 @@ docker run -d \
   --link elk_elasticsearch:elasticsearch \
   --name elk_logstash \
   -e LOGSPOUT=ignore \
-  logstash-with-config:5.1.1
+  logstash-with-config:5.2.0
 
 sleep 1
 echo 'Cluster started'
